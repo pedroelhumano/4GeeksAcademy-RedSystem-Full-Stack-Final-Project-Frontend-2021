e9c1ea25e4b9d66954a550bc21fce7b255e9cfaf
Pedro Yanez <pedro_y91@outlook.com>
2021-08-24 21:32:09 +0000

    se convierte en componente ver fotoso orden

--- src/front/js/pages/ver_fotos_orden.js
+++ src/front/js/pages/ver_fotos_orden.js
@@ -3,6 +3,9 @@ import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 import "../../styles/app.scss";
 
+//Componentes importados
+import { Imagesfromgallery } from "../component/imagesfromgallery.js";
+
 export const VerFotosOrden = () => {
 	const { store, actions } = useContext(Context);
 	const params = useParams();
@@ -19,116 +22,19 @@ export const VerFotosOrden = () => {
 			<div className="row container-fluid tab-content fondocuerpo m-0 p-0">
 				<div id="collageView" className="tab-pane fade show active">
 					<div className="d-flex flex-wrap bg-white">
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="https://lanoticia.cl/wp-content/uploads/2021/03/SEC-OHIGGINS-FISCALIZA-QUE-TRABAJOS-DE-CGE-DURANTE-CORTE4.jpg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="https://img.freepik.com/free-photo/electrical-linemam-worker-climb-bamboo-ladder-repair-wire-telecom-engineer-installing-wire-internet_157563-15.jpg?size=626&ext=jpg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="https://mk0nationaltodayijln.kinstacdn.com/wp-content/uploads/2020/04/national-lineman-appreciation-day-640x514.jpg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="http://www.spw.cl/IMG/jpg/pe-mufa4.jpg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="http://static.tiempo.com.mx/uploads/imagen/imagen/329232/principal_choque%20poste%2002.jpg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="http://www.spw.cl/IMG/jpg/pe-mufa1.jpg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="http://hmctec.pe/wp-content/uploads/2018/02/mufa-externa-punto-cero-1024x768.jpeg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="http://www.spw.cl/IMG/jpg/pe-caja1-detalle.jpg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="https://nextcable.com.br/wp-content/uploads/2020/05/RaekfNfCIJ.jpg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="https://i.imgur.com/2UCsnDr.jpeg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="https://thumbs.dreamstime.com/b/technician-wooden-ladder-installing-fiber-optic-internet-splitter-box-electric-pole-against-blue-sky-rear-view-197452160.jpg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="https://lanoticia.cl/wp-content/uploads/2021/03/SEC-OHIGGINS-FISCALIZA-QUE-TRABAJOS-DE-CGE-DURANTE-CORTE4.jpg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="https://img.freepik.com/free-photo/electrical-linemam-worker-climb-bamboo-ladder-repair-wire-telecom-engineer-installing-wire-internet_157563-15.jpg?size=626&ext=jpg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="https://mk0nationaltodayijln.kinstacdn.com/wp-content/uploads/2020/04/national-lineman-appreciation-day-640x514.jpg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="http://www.spw.cl/IMG/jpg/pe-mufa4.jpg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="http://static.tiempo.com.mx/uploads/imagen/imagen/329232/principal_choque%20poste%2002.jpg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="http://www.spw.cl/IMG/jpg/pe-mufa1.jpg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="http://hmctec.pe/wp-content/uploads/2018/02/mufa-externa-punto-cero-1024x768.jpeg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="http://www.spw.cl/IMG/jpg/pe-caja1-detalle.jpg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="https://nextcable.com.br/wp-content/uploads/2020/05/RaekfNfCIJ.jpg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="https://i.imgur.com/2UCsnDr.jpeg"
-							alt=""
-						/>
-						<img
-							className="img-fluid col-4 col-sm-4 col-md-3 col-lg-2 collageimg px-1 py-1"
-							src="https://thumbs.dreamstime.com/b/technician-wooden-ladder-installing-fiber-optic-internet-splitter-box-electric-pole-against-blue-sky-rear-view-197452160.jpg"
-							alt=""
-						/>
+						<Imagesfromgallery url_img="https://lanoticia.cl/wp-content/uploads/2021/03/SEC-OHIGGINS-FISCALIZA-QUE-TRABAJOS-DE-CGE-DURANTE-CORTE4.jpg" />
+						<Imagesfromgallery url_img="https://img.freepik.com/free-photo/electrical-linemam-worker-climb-bamboo-ladder-repair-wire-telecom-engineer-installing-wire-internet_157563-15.jpg?size=626&ext=jpg" />
+						<Imagesfromgallery url_img="https://mk0nationaltodayijln.kinstacdn.com/wp-content/uploads/2020/04/national-lineman-appreciation-day-640x514.jpg" />
+						<Imagesfromgallery url_img="http://www.spw.cl/IMG/jpg/pe-mufa4.jpg" />
+						<Imagesfromgallery url_img="http://static.tiempo.com.mx/uploads/imagen/imagen/329232/principal_choque%20poste%2002.jpg" />
+						<Imagesfromgallery url_img="http://www.spw.cl/IMG/jpg/pe-mufa1.jpg" />
+						<Imagesfromgallery url_img="http://hmctec.pe/wp-content/uploads/2018/02/mufa-externa-punto-cero-1024x768.jpeg" />
+						<Imagesfromgallery url_img="http://www.spw.cl/IMG/jpg/pe-caja1-detalle.jpg" />
+						<Imagesfromgallery url_img="https://i.imgur.com/2UCsnDr.jpeg" />
+						<Imagesfromgallery url_img="https://thumbs.dreamstime.com/b/technician-wooden-ladder-installing-fiber-optic-internet-splitter-box-electric-pole-against-blue-sky-rear-view-197452160.jpg" />
+						<Imagesfromgallery url_img="https://lanoticia.cl/wp-content/uploads/2021/03/SEC-OHIGGINS-FISCALIZA-QUE-TRABAJOS-DE-CGE-DURANTE-CORTE4.jpg" />
+						<Imagesfromgallery url_img="https://img.freepik.com/free-photo/electrical-linemam-worker-climb-bamboo-ladder-repair-wire-telecom-engineer-installing-wire-internet_157563-15.jpg?size=626&ext=jpg" />
+						<Imagesfromgallery url_img="https://mk0nationaltodayijln.kinstacdn.com/wp-content/uploads/2020/04/national-lineman-appreciation-day-640x514.jpg" />
 					</div>
 				</div>
 			</div>
